6adb59d61360fa47d4cae7a82477599454a2c4d6
Sadnan-2019 <sadnan.wow@gmail.com>
2022-11-02 16:46:30 +0600

    about

--- src/components/Aboutus/AboutDetails.js
+++ src/components/Aboutus/AboutDetails.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import career from "../../assets/carreer.png"
+import React from "react";
+import career from "../../assets/carreer.png";
 const AboutDetails = () => {
-    return (
-        <div>
-            <div>
-      <h2 className=" text-center  sm:text-4xl lg:text-[43px] md:text-[30px] font-bold text-neutral py-8">
-        About
-      </h2>
+  return (
+    <div>
+      <div>
+        <h2 className=" text-center  sm:text-4xl lg:text-[43px] md:text-[30px] font-bold text-neutral py-8">
+          About
+        </h2>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 px-12 gap-10  items-center justify-items-center">
-        <div>
-          <p className="text-neutral text-1xl p-5">
-            We consider the people of MRG GROUP to be our greatest asset. We
-            serve the needs of our businesses by implementing key hiring
-            processes and creating a performance-driven culture for the group’s
-            companies.
-          </p>
-          <p className="text-neutral text-1xl p-5">
-            We attract, develop and retain talent, ensuring employee inclusion
-            and engagement. Throughout our work, we strive to deliver clear,
-            simple and effective services for the well-being of our people.
-          </p>
-          <p className="text-neutral text-1xl p-5">
-            If you have the expertise and the desire to join MRG GROUP, click
-            the button below for possibilities and opportunities.
-          </p>
-           
-        </div>
-        <div>
-          <img src={career} alt=""/>
+        <div className="grid grid-cols-1 lg:grid-cols-2 px-12 gap-10  items-center justify-items-center">
+          <div>
+            <p className="text-neutral text-1xl p-5">
+              MRG Group of Companies was established in the year 1998 and
+              started its commercial operation in mid 1999.
+            </p>
+            <p className="text-neutral text-1xl p-5">
+              Within the short period of time, it has built confidence to the
+              customers of renowned brands worldwide. Root Group is a premium
+              denim producer and well-known name in the denim industry. It’s a
+              vertical unit, which is capable of making denim fabric to garments
+              as its final output. The yarn is being made at our own spinning
+              mill, and denim fabric and garments under Root Apparels Ltd-all
+              are under the same roof & same big name of Root Group of
+              Companies.
+            </p>
+            <p className="text-neutral text-1xl p-5">
+              The entire sister concerns of MRG Group are well equipped with
+              most modern laboratories, machineries, equipment and facilities.
+              Total numbers of corporate employees and workers are 4500. The
+              group runs under the proven leadership of Mr. Mohammed Razzakul
+              Hossen Tutul and comprises management body and employees with long
+              experience and proven track record in this sector.
+            </p>
+          </div>
+          <div>
+            <img src={career} alt="" />
+          </div>
         </div>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default AboutDetails;
