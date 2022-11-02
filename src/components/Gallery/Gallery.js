80d854133c2589eac907728959b27c8c40636d70
Sadnan-2019 <sadnan.wow@gmail.com>
2022-11-02 11:35:02 +0600

    effectt

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import "./Gallery.css"
+import GalleryLanding from './GalleryLanding';
+
 
 const Gallery = () => {
     return (
         <div>
-                       <div className="hero min-h-screen gallery"  >
-  <div className="  "></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Gallery</h1>
-      <p className="mb-5">MRG Group of Companies is one of the largest conglomerates in Bangladesh, Specialized in the Healthcare, Education, Convention Center & Advanced Poultry/ Fish Feeds Sector.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>  
+      <GalleryLanding/>
         </div>
     );
 };
---
+++ src/components/Gallery/GalleryLanding.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Gallery.css"
+const GalleryLanding = () => {
+    return (
+        <div>
+                             <div className="hero min-h-screen gallery"  >
+  <div className="  "></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Gallery</h1>
+      <p className="mb-5">MRG Group of Companies is one of the largest conglomerates in Bangladesh, Specialized in the Healthcare, Education, Convention Center & Advanced Poultry/ Fish Feeds Sector.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>  
+        </div>
+    );
+};
+
+export default GalleryLanding;
