1b22dba6dc635eecea85bef7c7d1e0d137d03cf4
Sadnan-2019 <sadnan.wow@gmail.com>
2022-11-05 17:10:17 +0600

    about

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import one from "./../../assets/download.jpg" 
+import achivtwo from "./../../assets/achivtwo.jpg" 
 const Achievements = () => {
   return (
     <div>
@@ -9,16 +10,16 @@ const Achievements = () => {
 
       <div className="grid grid-sm-1  lg:grid-cols-3   px-12 gap-10 mb-10">
         <div>
-          <img src="https://www.akij.net/img/achievement-single.png" className="p-2        hover:  transition duration-500 hover:scale-110" alt="" />
+          <img src={one} className="p-2        hover:  transition duration-500 hover:scale-110" alt="" />
         </div>
         <div >
-          <img src="https://www.akij.net/img/ach-2.png" className="p-2 hover:  transition duration-500 hover:scale-110" alt="" />
-          <img src="https://www.akij.net/img/ach-4.png" className="p-2 hover:  transition duration-500 hover:scale-110" alt="" />
+          <img src={achivtwo} className="p-2 hover:  transition duration-500 hover:scale-110" alt="" />
+          <img src={achivtwo} className="p-2 hover:  transition duration-500 hover:scale-110" alt="" />
         </div>
 
         <div>
-          <img src="https://www.akij.net/img/ach-3.png" className="p-2 hover:  transition duration-500 hover:scale-110" alt="" />
-          <img src="https://www.akij.net/img/achievement-multiple.png" className="p-2 hover:  transition duration-500 hover:scale-110" alt="" />
+          <img src={achivtwo}className="p-2 hover:  transition duration-500 hover:scale-110" alt="" />
+          <img src={achivtwo} className="p-2 hover:  transition duration-500 hover:scale-110" alt="" />
         </div>
       </div>
     </div>
