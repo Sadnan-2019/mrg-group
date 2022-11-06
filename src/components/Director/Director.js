a5eca6077bb54fa84fed07f739da5efab336fa32
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-11-07 01:42:35 +0600

    side nav

--- src/components/Director/Director.js
+++ src/components/Director/Director.js
@@ -32,7 +32,29 @@ const Director = () => {
             <p>CEO</p>
           </div>
         </div>
-        <div
+      <div
+          className="card sm:w-5/6      bg-gradient-to-r from-[#303640] to-[#103264] hover:from-[#F39E4B] hover:to-[#4a473d]  lg:w-7/12 flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10 "
+          data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-delay="5000"
+        data-aos-duration="3000"
+          
+          style={{ height: "300px" }}
+        >
+          <figure className="">
+            <img
+              src={avater}
+              alt="Shoes"
+              className="  p-4  "
+              style={{ height: "150px", borderRadius: "25px" }}
+            />
+          </figure>
+          <div className=" items-center text-center text-white ">
+            <h2 className=" text-center font-bold">DR. Zahiruddin Mahmud </h2>
+            <p>CEO</p>
+          </div>
+        </div>
+        {/* <div
           className="card  sm:w-5/6     bg-gradient-to-r from-[#303640] to-[#103264] hover:from-[#F39E4B] hover:to-[#4a473d]  lg:w-7/12 flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10 "
           data-aos="fade-left"
         data-aos-offset="300"
@@ -52,7 +74,7 @@ const Director = () => {
             <h2 className=" text-center font-bold">DR. Zahiruddin Mahmud new </h2>
             <p>CEO</p>
           </div>
-        </div>
+        </div> */}
 
         </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-12 gap-10    py-[25px] items-center justify-items-center">
