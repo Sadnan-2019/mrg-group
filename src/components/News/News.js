c099c33b2a8afee87b89598da810404500956d09
Sadnan-2019 <sadnan.wow@gmail.com>
2022-11-16 13:21:13 +0600

    gon on

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -28,15 +28,12 @@ const News = () => {
             </figure>
             <div className="h-4/5 items-center text-center bg-gradient-to-r from-[#303640] to-[#103264] text-white p-4  ">
               <h2 className=" text-justify ">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged.
+                For the people of mymenshing division we have opened up a branch of our physiotherapy center-
               </h2>
             </div>
           </div>
+
+
           <div
             className="card  border w-full h-full    "
             data-aos="flip-left"
--- src/components/NewsMedia/MoreNews.js
+++ src/components/NewsMedia/MoreNews.js
@@ -35,6 +35,33 @@ const MoreNews = () => {
               </h2>
             </div>
           </div>
+        <div
+            className="card  border w-full h-full    "
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+            style={{ height: "500px" }}
+          >
+            <figure className="">
+              <img
+                src="https://i.ibb.co/S0LPY46/tulipres.jpg"
+                alt="Shoes"
+                className=" w-full  p-4  hover:  transition duration-500 hover:scale-110 "
+                style={{  height:"250px",borderRadius: "25px" }}
+              />
+              {/* <img src="https://i.ibb.co/QcMVHtK/9.png" className="p-2        hover:  transition duration-500 hover:scale-110" alt="" /> */}
+            </figure>
+            <div className="h-4/5 items-center text-center bg-gradient-to-r from-[#303640] to-[#103264] text-white p-4  ">
+              <h2 className=" text-justify ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+              </h2>
+            </div>
+          </div>
         </div>
       </div>
     </div>
